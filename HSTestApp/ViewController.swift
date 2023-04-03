9636834da1c70626f1629b2e733486f99da24af8
rvronski <rvronski@gmail.com>
2023-04-03 16:55:41 +0500

    Initial Commit

---
+++ HSTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSTestApp
+//
+//  Created by ROMAN VRONSKY on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
